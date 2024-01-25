c962360ed37a99553b3572829bb2861d7e93ea42
kky <kykim9050@naver.com>
2024-01-25 15:57:22 +0900

    김경윤 : Map 클래스 추가

---
+++ PvZ_KKY/PvZContents/Map.cpp
@@ -0,0 +1,11 @@
+﻿#include "Map.h"
+
+Map::Map()
+{
+}
+
+Map::~Map()
+{
+}
+
+
---
+++ PvZ_KKY/PvZContents/Map.h
@@ -0,0 +1,22 @@
+#pragma once
+
+// Ό³Έν :
+class Map
+{
+public:
+	// constrcuter destructer
+	Map();
+	~Map();
+
+	// delete Function
+	Map(const Map& _Other) = delete;
+	Map(Map&& _Other) noexcept = delete;
+	Map& operator=(const Map& _Other) = delete;
+	Map& operator=(Map&& _Other) noexcept = delete;
+
+protected:
+
+private:
+
+};
+
